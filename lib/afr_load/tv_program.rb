2db2f6791887d4e8634d96f93542f7dd14e7b28b
iaia <iaia72160@gmail.com>
2016-10-16 02:18:51 +0900

    add class tv_program

---
+++ lib/afr_load/tv_program.rb
@@ -0,0 +1,27 @@
+# encoding: utf-8
+
+module TvProgram
+    class TvProgram
+        def on_air_date
+            @on_air_date
+        end
+        def title_ja
+            @title_ja
+        end
+        def title
+            @title
+        end
+        def released_year
+            @released_year
+        end
+        def released_country
+            @released_country
+        end
+        def leading_actor
+            @leading_actor
+        end
+        def supporting_actor
+            @supporing_actor
+        end
+    end
+end
